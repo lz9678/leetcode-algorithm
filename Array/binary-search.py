3c9048992d8c60376a76a2b094b1e5c7465943ab
lin <lin.zhang9678@gmail.com>
2023-09-09 00:52:27 -0400

    update the file

--- Array/binary-search.py
+++ Array/binary-search.py
@@ -1,3 +1,9 @@
+'''
+Problem Statement:
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+'''
 
 # Method 1: 
 class Solution:
@@ -45,4 +51,3 @@ class Solution:
 			else:
 				left = middle+1
 		return -1
-
